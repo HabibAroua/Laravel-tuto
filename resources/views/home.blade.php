880c5701fda4ec90433968b75441d2033b57c024
Habib Aroua <HabibAroua@users.noreply.github.com>
2021-01-22 21:57:36 +0100

    update the scss files

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,9 +9,9 @@
         <div class="col-9 pt-5">
             <div><h1>Maker akadmey</h1></div>
             <div class="d-flex">
-                <div class="pr-3"><strong>153</strong>posts</div>
-                <div class="pr-3"><strong>23k</strong>followers</div>
-                <div class="pr-3"><strong>212</strong>following</div>
+                <div class="pr-5"><strong>153</strong>posts</div>
+                <div class="pr-5"><strong>23k</strong>followers</div>
+                <div class="pr-5"><strong>212</strong>following</div>
             </div>
         </div>
     </div>
