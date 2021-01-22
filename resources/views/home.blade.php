980d927b4e111f9a4eb999d8381fbf33ad16a6b1
Habib Aroua <HabibAroua@users.noreply.github.com>
2021-01-22 22:24:20 +0100

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,30 @@
                 <div class="pr-5"><strong>23k</strong>followers</div>
                 <div class="pr-5"><strong>212</strong>following</div>
             </div>
+            <div class="pt-4 font-weight-bold">Markers</div>
+            <div>
+                Soy coder && amo con todo mi amor a soy luna(karol sevilla & ruggero pascuarelli) 😍 siguanme también amo a los Chavis de cd9 gracias 👉✌👈😂
+            </div>
+            <div>
+                <a href="#">www.makers.com</a>
+            </div>
         </div>
+        <div class="row pt-5">
+            <div class="col-4">
+                <img src="/svg/makers.svg" alt="" class="w-100" />
+            </div>
+            <div class="col-4">
+                <img src="/svg/makers.svg" alt="" class="w-100" />
+            </div>
+            <div class="col-4">
+                <img src="/svg/makers.svg" alt="" class="w-100"/>
+            </div>
+            <div class="col-4">
+                <img src="/svg/makers.svg" alt="" class="w-100" />
+            </div>
+
+        </div>
+
     </div>
 </div>
 @endsection
