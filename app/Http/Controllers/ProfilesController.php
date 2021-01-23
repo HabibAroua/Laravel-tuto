ae90ee27761dbbc81d70be3385b20369e7c0635f
Habib Aroua <HabibAroua@users.noreply.github.com>
2021-01-23 15:44:28 +0100

    update the controller

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class ProfilesController extends Controller
 {
-    //
+    public function index()
+    {
+        $x = 2;
+        if($x == 2)
+            return view('home');
+        else
+            return view('welcome');
+    }
 }
